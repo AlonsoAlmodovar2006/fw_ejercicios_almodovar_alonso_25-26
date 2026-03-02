a243c379ee60993be004417bf6aa12b92e31dd90
Alonso Almodóvar <alonsoalmodovar09@gmail.com>
2026-03-02 10:47:04 +0100

    Ejercicios Apuntes

--- fw_t02p02_almodovar_alonso/ejercicio3_meals_almodovar/src/app/app.config.ts
+++ fw_t02p02_almodovar_alonso/ejercicio3_meals_almodovar/src/app/app.config.ts
@@ -7,7 +7,7 @@ import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    // provideHttpClient(),
+    provideHttpClient(),
     provideRouter(routes)
   ]
 };
---
+++ fw_t04p01_almodovar_alonso/api_node_01/src/routes/api.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+
+// Delegamos en places.js
+router.use("/places", require("./api/places"));
+
+//Delegamos a products.js
+router.use("/products", require("./api/products"));
+
+//Delegamos a users.js
+router.use("/users", require("./api/users"));
+
+
+router.use("/auth", require("./api/auth"));
+
+module.exports = router;
